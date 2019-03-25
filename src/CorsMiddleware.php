4ac89654bb18ce92b717792d20bc04a02ae0a7e7
frank <frank@qlimix.net>
2019-03-25 15:15:30 +0100

    using actual cors

--- src/CorsMiddleware.php
+++ src/CorsMiddleware.php
@@ -2,6 +2,9 @@
 
 namespace Qlimix\HttpMiddleware;
 
+use Neomerx\Cors\Contracts\AnalysisResultInterface;
+use Neomerx\Cors\Contracts\AnalyzerInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
@@ -9,14 +12,47 @@ use Psr\Http\Server\RequestHandlerInterface;
 
 final class CorsMiddleware implements MiddlewareInterface
 {
+    /** @var AnalyzerInterface */
+    private $analyzer;
+
+    /** @var ResponseFactoryInterface */
+    private $response;
+
+    public function __construct(AnalyzerInterface $analyzer, ResponseFactoryInterface $response)
+    {
+        $this->analyzer = $analyzer;
+        $this->response = $response;
+    }
+
     /**
      * @inheritdoc
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        return $handler->handle($request)
-            ->withAddedHeader('Access-Control-Allow-Origin', '*')
-            ->withAddedHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS, DELETE, PATCH')
-            ->withAddedHeader('Access-Control-Allow-Headers', 'DNT, X-CustomHeader, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-Type, Authorization');
+        $result = $this->analyzer->analyze($request);
+
+        switch ($result->getRequestType()) {
+            case AnalysisResultInterface::ERR_NO_HOST_HEADER:
+            case AnalysisResultInterface::ERR_ORIGIN_NOT_ALLOWED:
+            case AnalysisResultInterface::ERR_METHOD_NOT_SUPPORTED:
+            case AnalysisResultInterface::ERR_HEADERS_NOT_SUPPORTED:
+                return $this->response->createResponse(403);
+            case AnalysisResultInterface::TYPE_PRE_FLIGHT_REQUEST:
+                return $this->addCorsHeaders($this->response->createResponse(204), $result);
+            case AnalysisResultInterface::TYPE_REQUEST_OUT_OF_CORS_SCOPE:
+                return $handler->handle($request);
+            default:
+                return $this->addCorsHeaders($handler->handle($request), $result);
+        }
+    }
+
+    private function addCorsHeaders(ResponseInterface $response, AnalysisResultInterface $result): ResponseInterface
+    {
+        $corsHeaders = $result->getResponseHeaders();
+        foreach ($corsHeaders as $name => $value) {
+            $response->withAddedHeader($name, $value);
+        }
+
+        return $response;
     }
 }
